e15703493bce0aa60808a75358e54bdf99472b4b
anouar-amz <anouaramzane@gmail.com>
2023-03-21 11:59:48 -0700

    50 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that prints the alphabet 10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int j;
+
+	for (j = 0; j < 50; j++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
