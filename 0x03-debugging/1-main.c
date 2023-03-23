ebb39c86b1ff418ec9833b6ca19bf4c21fbab8d7
anouar-amz <anouaramzane@gmail.com>
2023-03-23 14:17:58 -0700

    deb

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -17,12 +17,10 @@ int main(void)
 	{
 		putchar(i);
 	}
-	/**
-	 * while (i < 10)
-	 * {
-	 * putchar (i);
-	 * }
-	 */
+	/* while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
