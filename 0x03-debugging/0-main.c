2f305406cf1b96941320c627753b0a3d8072a925
anouar-amz <anouaramzane@gmail.com>
2023-03-23 13:50:02 -0700

    main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests fucntion that prints if an integer is 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	if (i == 0)
+	{
+		positive_or_negative(i);
+	}
+	return (0);
+}
