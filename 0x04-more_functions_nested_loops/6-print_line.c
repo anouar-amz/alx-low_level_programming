5e0bf96a599678241a349dcd8c30a2cf29234891
anouar-amz <anouaramzane@gmail.com>
2023-03-23 18:06:00 -0700

    farfar

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times character _ should be printed
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < n; a++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
