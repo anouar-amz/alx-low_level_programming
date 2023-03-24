80dc7cd5fc5b0dac4509055b22271866ebe6da7f
anouar-amz <anouaramzane@gmail.com>
2023-03-23 18:30:50 -0700

    xD

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,7 +7,7 @@
 
 void print_diagonal(int n)
 {
-	int x;
+	int x, y;
 
 	if (n <= 0)
 	{
@@ -17,8 +17,12 @@ void print_diagonal(int n)
 	{
 		for (x = 0; x < n; x++)
 		{
+			for (y = 0; y < x; y++)
+			{
+				_putchar(' ');
+			}
 			_putchar(92);
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
